40fb212ab82b7e9da0e81362e25c8e563b9c3422
Hariom Chouhan <hariomchouhan430@gmail.com>
2023-08-01 16:07:47 +0530

    Complete Left Category Section and move to the Feed Section

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import LeftNav from './LeftNav'
 const Feed = () => {
+
   return (
-    <div>
-      Feed
+    <div className='flex flex-row h-[calc(100%-56px)]'>
+      <LeftNav />
     </div>
   )
 }
--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { categories } from "../utils/constants";
+import LeftNavMenuItem from "./LeftNavMenuItem";
+import { AppContext } from "../context/ContextApi";
+import { useNavigate } from "react-router-dom";
 
 const LeftNav = () => {
-  return (
-    <div>
-      LeftNav
-    </div>
-  )
-}
+  const { selectCategories, setSelectCategories, mobileMenu } =
+    useContext(AppContext);
 
-export default LeftNav
+  const navigate = useNavigate();
+  const clickHandler = (name, type) => {
+    switch (type) {
+      case "category":
+        return setSelectCategories(name);
+      case "home":
+        return setSelectCategories(name);
+      case "menu":
+        return false;
+      default:
+        break;
+    }
+  };
+  return (
+    <div className="md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240] md:translate-x-0 transition-all">
+      <div className="flex flex-col px-5">
+        {categories.map((item, index) => (
+          <React.Fragment key={index}>
+            <LeftNavMenuItem
+              text={item.type === "home" ? "Home" : item.name}
+              icon={item.icon}
+              action={() => {
+                clickHandler(item.name, item.type);
+                navigate("/");
+              }}
+              className={`${
+                selectCategories === item.name ? "bg-white/[0.15]" : ""
+              }`}
+            />
+            {item.divider && <hr className="my-5 border-white/[0.2]" />}
+          </React.Fragment>
+        ))}
+        <hr className="my-5 border-white/[0.2]" />
+        <div className="text-white/[0.5] text-[12px]">Clone by: Hari ॐ</div>
+      </div>
+    </div>
+  );
+};
+
+export default LeftNav;
